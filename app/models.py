db6c5a756658e9539493ad64f9686e6a9380b0f0
e11lwc <e11lwc@abc>
2020-04-20 16:21:03 +0800

    add instance table account table bot_state table

--- app/models.py
+++ app/models.py
@@ -29,6 +29,16 @@ class bbl_guild_t(db.Model):
     def __repr__(self):
         return '<BBL Guild_t %r>' % self.name
 
+class bbl_instance_t(db.Model):
+    __tablename__ = 'bbl_instance_t'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    game_id = db.Column(db.String(16))
+    team_id = db.Column(db.Integer())
+
+    def __repr__(self):
+        return '<BBL Instance_t %r>' % self.name
+
 class moe_cmd_t(db.Model):
     __tablename__ = 'moe_cmd_t'
     cmd_id = db.Column(db.Unicode(128), primary_key=True)
@@ -57,3 +67,30 @@ class moe_guild_t(db.Model):
 
     def __repr__(self):
         return '<MOE Guild_t %r>' % self.name
+
+class moe_instance_t(db.Model):
+    __tablename__ = 'moe_instance_t'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    game_id = db.Column(db.String(16))
+    team_id = db.Column(db.Integer())
+
+    def __repr__(self):
+        return '<MOE Instance_t %r>' % self.name
+
+class account_t(db.Model):
+    __tablename__ = 'account_t'
+    id = db.Column(db.Integer(), primary_key=True)
+    line_uid = db.Column(db.String(256))
+    pority = db.Column(db.Integer())
+
+    def __repr__(self):
+        return '<Account_t %r' % self.name
+
+class bot_state(db.Model):
+    __tablename__ = 'bot_state_t'
+    item_text = db.Column(db.String(64), primary_key=True)
+    item_value = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Bot State_t %r>' % self.name
